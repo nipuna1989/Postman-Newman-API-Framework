865802c277a4814f18d51b3450104cea11278945
Nipuna-Chairsyde <nipuna.j@chairsyde.com>
2025-07-23 10:56:52 +0200

    resolve issues with report creation

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Create reports directory if it doesn't exist
+mkdir -p reports
+
 echo "Running Remwaste API Tests"
 
 newman run collections/remwaste-api-framework.postman_collection.json \
   -e "environments/RemwasteENV.postman_environments.json" \
   --iteration-data data/login-cases.json \
-  --reporters cli,json,html \
-  --reporter-html-export reports/remwaste-report.html \
+  --reporters cli,htmlextra,json \
+  --reporter-htmlextra-export reports/remwaste-report.html \
   --reporter-json-export reports/remwaste-report.json
